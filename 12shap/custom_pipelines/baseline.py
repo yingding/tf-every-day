0f5ee5d3b44a54a4a8915052a5bf499c9d8d4b7a
yingding <yingdingwang@yahoo.de>
2023-02-28 18:01:24 +0100

    update shap plot for XGBRegressor

--- 12shap/custom_pipelines/baseline.py
+++ 12shap/custom_pipelines/baseline.py
@@ -1,6 +1,46 @@
-from zenml.steps import step
+import numpy as np
+from zenml.steps import Output, step
+from sklearn.datasets import load_diabetes
+from pandas import DataFrame, Series
+import xgboost
+from xgboost.sklearn import XGBRegressor
+
+@step
+def load_data() -> Output(
+    X_train=DataFrame, 
+    y_train=Series
+):
+    """Load boston hausing data"""
+    X_train, y_train = load_diabetes(return_X_y=True, as_frame=True)
+    # print(type(X_train))
+    # (442, 10)
+    # print(type(y_train))
+    # (442,)
+    return X_train, y_train
 
 @step
 def baseline_trainer(
-) -> None:
-    pass 
+    X_train: DataFrame,
+    y_train: Series
+) -> XGBRegressor:
+    # print(f"X_train type {type(X_train)}")
+    # print(X_train.shape)
+    # print(f"y_train type {type(y_train)}")
+    # print(y_train.shape) 
+    print(X_train.head())
+    print(y_train.head())
+    model = xgboost.XGBRegressor().fit(X_train, y_train)
+    return model
+
+
+@step 
+def shap_explainer(X_train: DataFrame, model: XGBRegressor) -> None:
+    import shap
+    explainer = shap.Explainer(model)
+    shap_values = explainer(X_train)
+    # shap.plots.waterfall(shap_values[0])
+    # shap.plots.force(shap_values)
+    print(type(shap_values))
+    print(shap_values[0, "bmi"])
+    shap.plots.scatter(shap_values[:,"bmi"], color=shap_values)
+
--- 12shap/custom_pipelines/example_pipeline.py
+++ 12shap/custom_pipelines/example_pipeline.py
@@ -1,13 +1,18 @@
 from zenml.pipelines import pipeline
 from util import get_local_time_str
-from baseline import baseline_trainer
+from baseline import baseline_trainer, load_data, shap_explainer
+from xgboost.sklearn import XGBRegressor
 
 @pipeline
-def my_pipeline(step_1):
-    step_1()
+def my_pipeline(step_1, step_2, step_3):
+    X_train, y_train = step_1()
+    base_regressor: XGBRegressor = step_2(X_train, y_train)
+    step_3(X_train, base_regressor)
 
 my_pipeline_instance = my_pipeline(
-    step_1=baseline_trainer(),
+    step_1=load_data(),
+    step_2=baseline_trainer(),
+    step_3=shap_explainer()
 )
 
 # https://docs.zenml.io/starter-guide/pipelines
