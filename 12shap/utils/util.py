74193af4a5b287b364279312edefea4d3958c191
yingding <yingdingwang@yahoo.de>
2023-03-17 14:03:45 +0100

    updated data description

--- 12shap/utils/util.py
+++ 12shap/utils/util.py
@@ -2,8 +2,9 @@ import os
 import re
 from dataclasses import dataclass
 import pandas as pd
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from typing import Tuple
+from numpy import ndarray
 
 def current_dir():
     """get the absolute path of the current file directory"""
@@ -22,15 +23,23 @@ class KaggleData:
     train_path: str = ""
     test_path: str = ""
             
-    def load(self) -> Tuple[DataFrame, DataFrame]:
+    def _load(self) -> Tuple[DataFrame, DataFrame]:
         try:
-            train_df = pd.read_csv(self.train_path, sep=",", header=0)
-            test_df = pd.read_csv(self.test_path, sep=",", header=0)
+            train_X_y_df = pd.read_csv(self.train_path, sep=",", header=0)
+            test_X_df = pd.read_csv(self.test_path, sep=",", header=0)
         except: 
-            train_df = pd.DataFrame()
-            test_df = pd.DataFrame() 
-        return train_df, test_df
+            train_X_y_df = pd.DataFrame()
+            test_X_df = pd.DataFrame() 
+        return train_X_y_df, test_X_df
     
-    def load_all(self) -> DataFrame:
-        train_df, test_df = self.load()
-        return pd.concat([train_df, test_df], ignore_index=True) 
+    def load(self, label_col="Survived") -> Tuple[DataFrame, DataFrame, Series]:
+        train_X_y_df, test_X_df = self._load()
+        # train_X_y_df and test_X_df may have different size of column,
+        # thus the mask must be calculated separately
+        train_column_mask: ndarray = ~train_X_y_df.columns.isin([label_col])
+        test_column_mask: ndarray = ~test_X_df.columns.isin([label_col])
+        return train_X_y_df.loc[:, train_column_mask], test_X_df.loc[:, test_column_mask], train_X_y_df[label_col]
+    
+    def load_all(self, label_col="Survived") -> DataFrame:
+        train_X_df, test_X_df, _ = self.load(label_col)
+        return pd.concat([train_X_df, test_X_df], ignore_index=True) 
